e1904259a438fc5f124841ca5b5ff7453ee01607
Raghu-Nandan-Sharma-19 <raghunandansharma1905@gmail.com>
2024-11-21 11:32:54 +0530

    Updated tailwind css

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const TaskList = () => {
   return (
-    <div id='tasklist' className='h-[55%] flex  flex-row overflow-x-auto items-center justify-start gap-5 
+    <div id='tasklist' className='h-[55%] flex flex-col lg:flex-row lg:overflow-x-auto items-center justify-start gap-5 
     flex-nowrap w-full py-5 mt-10 '>
-        <div className='h-full flex-shrink-0 w-[80%] p-5 lg:w-[300px] rounded-xl bg-red-400'>
+        <div className='h-full flex-shrink-0 w-full p-5 lg:w-[300px] rounded-xl bg-red-400'>
             <div className='flex justify-between items-center'>
                 <h3 className='bg-red-600 px-3 py-1 text-sm rounded'>High</h3>
                 <h4 className='text-sm'>20 Feb 2024</h4>
